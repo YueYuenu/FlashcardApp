0744cd7d55fbb1ff24956fbf5a28a2cc272e2acc
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2023-11-14 15:56:56 +0100

    tests for GetCardDeckByFlashcardId

---
+++ FlashcardApp.Tests/DeckServiceTests/GetCardDeckByFlashcardId.cs
@@ -0,0 +1,19 @@
+﻿using FlashcardApp.Domain.Models;
+
+namespace FlashcardApp.Tests.DeckServiceTests
+{
+    public class GetCardDeckByFlashcardId : Base
+    {
+        [Fact]
+        public void Get_CardDeck_By_Flashcard_Id()
+        {
+            //Arrange
+            _deckRepo.Setup(x => x.GetCardDeckByFlashcardId(1)).Returns(new CardDeck() { DeckId = 2, DeckName = "testdeck" });
+
+            //Act
+            CardDeck founddeck = _deckService.GetCardDeckByFlashcardId(1);
+            //Assert
+            Assert.Equal(2, founddeck.DeckId);
+        }
+    }
+}
