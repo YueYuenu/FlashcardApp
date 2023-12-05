ae7f6c71c1b580cc477a15943a0c0c2b7a967678
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2023-12-05 15:48:08 +0100

    redid some routing and activated @input

--- angularapp/src/app/app-routing/app-routing.module.ts
+++ angularapp/src/app/app-routing/app-routing.module.ts
@@ -9,23 +9,27 @@ import { PageNotFoundComponent } from '../page-not-found/page-not-found.componen
 import { ListDecksComponent } from '../list-decks/list-decks.component';
 import { EditCardDeckComponent } from '../edit-card-deck/edit-card-deck.component';
 import { AddDeckComponent } from '../add-deck/add-deck.component';
+import { DeckInfoComponent } from '../deck-info/deck-info.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  // Flashcards
   { path: 'flashcardslist', component: ListFlashcardsComponent },
-  { path: 'add-flashcards', component: AddFlashcardsComponent },
-  { path: 'add-deck', component: AddDeckComponent},
-  { path: 'decklist', component: ListDecksComponent},
-  { path: 'carddetails/:id', component: EditFlashcardsComponent },
-  { path: 'deckdetails/:id', component: EditCardDeckComponent},
+  { path: 'deckdetails/:deckId/add-flashcards', component: AddFlashcardsComponent },
+  { path: 'carddetails/:cardid', component: EditFlashcardsComponent },
   { path: 'deleteflashcards', component: DeleteFlashcardsComponent },
+  // Decks
+  { path: 'decklist', component: ListDecksComponent },
+  { path: 'deckdetails/:deckId', component:DeckInfoComponent },
+  { path: 'add-deck', component: AddDeckComponent },
+  { path: 'deckdetails/:deckId/editdeck', component: EditCardDeckComponent },
   { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
 
 ]
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { bindToComponentInputs: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
