67faed9f07ddfae40d5e6b71db5190e99b65ee0f
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2023-12-05 15:49:13 +0100

    changed so it now works from the assigned deck

--- angularapp/src/app/add-flashcards/add-flashcards.component.ts
+++ angularapp/src/app/add-flashcards/add-flashcards.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FlashcardService } from '../services/flashcard.service';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar as MatSnackBar } from '@angular/material/snack-bar';
 import Flashcard from '../models/Flashcard';
+import { DeckService } from '../services/deck.service';
+import CardDeck from '../models/CardDeck';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-add-flashcards',
@@ -10,26 +13,47 @@ import Flashcard from '../models/Flashcard';
   styleUrls: ['./add-flashcards.component.css']
 })
 export class AddFlashcardsComponent implements OnInit {
+  @Input() deckId!: number;
   cardForm!: FormGroup;
   cardAdd!: Flashcard;
+  deck$?: Observable<CardDeck>;
+  deckName: any;
 
-  constructor(private flashcardService: FlashcardService, private formbuilder: FormBuilder, private _snackbar: MatSnackBar) {
+  constructor(private flashcardService: FlashcardService, private deckService: DeckService, private formbuilder: FormBuilder, private _snackbar: MatSnackBar) {
     this.cardForm = this.formbuilder.group({
+      deckId: new FormControl(),
       Question: new FormControl('', Validators.required),
       Answer: new FormControl('', Validators.required),
     })
   }
 
   ngOnInit(): void {
+    this.deck$ = this.deckService.GetDeckById(this.deckId);
+    console.log('check params id: ' + this.deckId)
+    this.deck$?.subscribe(deckpatch => {
+      this.cardForm.patchValue(
+        {
+          deckId: deckpatch.deckId
+        }
+      )
+      this.cardForm.patchValue(
+        {
+          deckName: deckpatch.deckName
+        }
+      )
+      this.deckName = deckpatch.deckName;
+      console.log('deckId check: ' + deckpatch.deckId, 'deckName: ' + deckpatch.deckName)
+    });
   }
 
   formSubmission() {
     this.cardForm
     this.cardAdd = {
+      deckId: this.deckId,
       question: this.cardForm.get("Question")?.value,
       answer: this.cardForm.get("Answer")?.value,
     };
-    console.log("card add", this.cardAdd) //question and answer are null in db but appear correctly in this log.
+    console.log("card add", this.cardAdd)
     this.flashcardService.AddCard(this.cardAdd).subscribe(res => {
       if (res.status == 200) { this._snackbar.open("Flashcard was successfully added.", "Close"); }
       else { this._snackbar.open("Something went wrong", "Close"); }
